67f904f7b27f3ac61a710486f866e6edd313c5e1
Houssem Rouabeh <81163379+HurricaneTN@users.noreply.github.com>
2022-10-25 17:44:10 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Profile } from "./Profile/Profile";
+import MyImage from "./Profile/me.jpg";
+import { Container, Navbar } from "react-bootstrap";
 
 function App() {
+  const user = {
+    fullName: "Houssem Rouabeh",
+    bio: "Je suis Houssem Rouabeh la plupart d’entre vous me connaissent, évidement un étudiant comme vous tous, mais aussi pour celui qui ne me connait pas je suis un professionnel dans le domaine de l’IT.",
+    profession: "IT Manager",
+  };
+  const handleName = () => {
+    alert(user.fullName);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#faedf0" }}>
+      <Navbar bg="dark">
+        <Container>
+          <img
+            src={logo}
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+          <marquee style={{ color: "white", fontSize: "30px" }}>
+            Bienvenue sur mon portfolio, conçu avec React JS
+          </marquee>
+        </Container>
+      </Navbar>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <Profile user={user} handleName={handleName}>
+          <img alt="moi" src={MyImage}></img>
+        </Profile>
+      </div>
     </div>
   );
 }
---
+++ src/Profile/Profile.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+export const Profile = (props) => {
+  const myStyle = {
+    color: "black",
+    backgroundColor: "#f5e1c9",
+    padding: "10px",
+    fontFamily: "Arial",
+    width: "20rem",
+    textAlign: "center",
+  };
+  return (
+    <div>
+      <Card style={myStyle}>
+        {props.children}
+        <Card.Body>
+          <Card.Title style={{ fontSize: "30px", fontWeight: "bolder" }}>
+            {props.user.fullName}
+          </Card.Title>
+          <Card.Text>{props.user.bio}</Card.Text>
+          <Card.Title style={{ fontWeight: "bold" }}>
+            Ma Profession est : <br />
+            {props.user.profession}
+          </Card.Title>
+          <Button variant="secondary" onClick={props.handleName}>
+            Show my Name
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+Profile.propTypes = {
+  user: PropTypes.shape({
+    fullName: PropTypes.string.isRequired,
+    bio: PropTypes.string,
+    profession: PropTypes.string,
+  }),
+  handleName: PropTypes.func,
+};
+
+Profile.defaultProps = {
+  user: {
+    fullName: "Nom: Non defini",
+    bio: "Bio: Non defini",
+    profession: "Profession: Non defini",
+  },
+};
